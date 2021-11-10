23b386ec6648246bd409247b88baeddbc5abc189
sully <robertosullivan5@gmail.com>
2021-11-10 08:22:50 +0000

    lab 2 updated

--- week-2/Labs/lab2_ros.R
+++ week-2/Labs/lab2_ros.R
@@ -19,12 +19,32 @@ print(paste("confidence is 200 +/-", cl))
 print(paste("so either", 200-cl, "ml or ", 200+cl, "ml"))
 
 #ex2 hypothesis 
-#after sampling 100 bottles mean= 197ml
+#after sampling 100 bottles sample mean=197ml
 # mean poured is 200ml at 1% significance
 #state the null hypothesis
 #h1: mean > 199
 #h0: mean <= 199
-# for z score  #99%	z = 2.576 from table 1%=.99547 = 0.00453
-#1 sample z-test is rarly used becuase we don't know the population std dev
 
- hp <- 100 - 200 / 2/sqrt(100)
+#Once you get the sample statistics, you can determine the p-value 
+#through different methods. 
+#The most common methods are the T-score and Z-score for normal distributions.
+#https://www.educba.com/z-score-vs-t-score/
+# for z score x-mu/std dev #99%	z = 2.576 from table 1%=.99547 = 0.00453
+#1 sample z-test is rarely used because we don't know the population std dev
+
+hp <- 100 - 200 / 2/sqrt(100)
+hp
+
+#it's called a lower-tailed test
+#if h0: mu >= 150
+#if h1: mu < 150
+
+#upper rail test
+#if h0: mu <= 500k
+#if h1: mu > 500k
+
+#2-Tailed Test
+#if h0: mu = 500k
+#if h1: mu != 25
+
+
